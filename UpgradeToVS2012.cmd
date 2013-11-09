00f2928578264400b9f8f58d0d33de640bf87a0f
dirk <dirk@git.imagemagick.org>
2013-11-09 12:29:44 +0000

    Changed FixVS2010.cmd to also upgrade the solution. Added cmd file to upgrade solution to VS2012.

---
+++ UpgradeToVS2012.cmd
@@ -0,0 +1,19 @@
+@echo off
+REM This script upgrades the solution to VS2012 and fixes the MSB8012 error.
+
+FOR /F "tokens=3" %%A IN ('REG QUERY "HKLM\SOFTWARE\Microsoft\PowerShell\1" /v Install ^| FIND "Install"') DO SET PowerShellInstalled=%%A
+
+IF NOT "%PowerShellInstalled%"=="0x1" GOTO NOT_INSTALLED
+
+call "%vs100comntools%vsvars32.bat"
+powershell -ExecutionPolicy Unrestricted .\build\UpgradeSolution.ps1 2010
+call "%vs110comntools%vsvars32.bat"
+powershell -ExecutionPolicy Unrestricted .\build\UpgradeSolution.ps1 2012
+
+GOTO DONE
+
+:NOT_INSTALLED
+ECHO "This script needs powershell"
+
+:DONE
+PAUSE
