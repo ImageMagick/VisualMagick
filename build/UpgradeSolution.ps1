2c4ed6f0e8200a51da30e0f5dc447b8a3e9ee5e8
dirk <dirk@git.imagemagick.org>
2013-11-09 12:30:23 +0000

    Forgot to copy file.

--- build/UpgradeSolution.ps1
+++ build/UpgradeSolution.ps1
@@ -1,20 +1,35 @@
+function CheckExitCode($msg)
+{
+	if ($LastExitCode -ne 0)
+	{
+		Write-Error $msg
+		Exit 1
+	}
+}
+
 function CreateTargetName($xml, $configuration)
 {
   $uri = "http://schemas.microsoft.com/developer/msbuild/2003"
 
-  [System.Xml.XmlNamespaceManager] $nsmgr = $xml.NameTable;
-  $nsmgr.AddNamespace("msb", $uri);
+  [System.Xml.XmlNamespaceManager] $nsmgr = $xml.NameTable
+  $nsmgr.AddNamespace("msb", $uri)
 
-  $propertyGroup = $xml.SelectSingleNode("//msb:PropertyGroup[contains(@Condition, '$configuration') and msb:OutDir]", $nsmgr)
+  $propertyGroup = $xml.SelectSingleNode("//msb:PropertyGroup[msb:OutDir]", $nsmgr)
   if ($propertyGroup -eq $null)
   {
     return
   }
 
-  $targetName = $propertyGroup.SelectSingleNode("msb:TargetName", $nsmgr)
+  $targetName = $propertyGroup.SelectSingleNode("msb:TargetName[contains(@Condition, '$configuration')]", $nsmgr)
   if ($targetName -ne $null)
   {
-    return;
+    return
+  }
+
+  $outDir = $propertyGroup.SelectSingleNode("msb:OutDir[contains(@Condition, '$configuration')]", $nsmgr)
+  if ($outDir -eq $null)
+  {
+    return
   }
 
   $outputFile = $xml.SelectSingleNode("//msb:ItemDefinitionGroup[contains(@Condition, '$configuration')]//msb:OutputFile", $nsmgr)
@@ -29,9 +44,35 @@ function CreateTargetName($xml, $configuration)
 
   $targetName = $xml.CreateElement("TargetName", $uri)
   $targetName.InnerText = $name
+
+  [void]$targetName.Attributes.Append($xml.CreateAttribute("Condition"))
+  $targetName."Condition" = $outDir."Condition"
+
   [void]$propertyGroup.AppendChild($targetName)
 
   [void]$outputFile.ParentNode.RemoveChild($outputFile)
+}
+
+function EnableMultiProcessorCompilation($xml)
+{
+  $uri = "http://schemas.microsoft.com/developer/msbuild/2003"
+
+  [System.Xml.XmlNamespaceManager] $nsmgr = $xml.NameTable
+  $nsmgr.AddNamespace("msb", $uri)
+
+  $clCompiles = $xml.SelectNodes("//msb:ClCompile", $nsmgr)
+  foreach ($clCompile in $clCompiles)
+  {
+    $multiProcessorCompilation = $clCompile.SelectSingleNode("msb:MultiProcessorCompilation", $nsmgr)
+    if ($multiProcessorCompilation -ne $null)
+    {
+      continue
+    }
+
+    $multiProcessorCompilation = $xml.CreateElement("MultiProcessorCompilation", $uri)
+    $multiProcessorCompilation.InnerText = "true"
+    [void]$clCompile.AppendChild($multiProcessorCompilation)
+  }
 }
 
 function FixProjectFile($fileName)
@@ -41,6 +82,7 @@ function FixProjectFile($fileName)
   $xml = [xml](get-content $fileName)
   CreateTargetName $xml "Debug"
   CreateTargetName $xml "Release"
+  EnableMultiProcessorCompilation $xml
   $xml.Save($fileName)
 }
 
@@ -52,4 +94,24 @@ function FixProjectFiles()
   }
 }
 
-FixProjectFiles
+function UpgradeSolution($fileName)
+{
+  Write-Host "Upgrading solution: $fileName"
+  devenv /upgrade $fileName
+  CheckExitCode "Upgrade failed."
+}
+
+function UpgradeSolutions()
+{
+  foreach ($fileName in [IO.Directory]::GetFiles(".", "*.sln"))
+  {
+    UpgradeSolution($fileName)
+  }
+}
+
+UpgradeSolutions
+
+if ($args[0] -eq 2010)
+{
+  FixProjectFiles
+}
