6be3e0f776ce86629f547a9f49cf655ef2856ae5
dirk <dirk@git.imagemagick.org>
2013-11-09 09:56:43 +0000

    Remove .bak file if it already exists.

--- configure/configure.cpp
+++ configure/configure.cpp
@@ -2089,6 +2089,7 @@ void CConfigureApp::process_project_replacements( const char *root,
                         renamed += "\\";
                         renamed += newstype;
                         s5 = renamed.c_str();
+                        DeleteFile(s5);
                         MoveFile(s1,s5);
                       }
                   }
