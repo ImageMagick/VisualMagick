1fb1be251ef75629b741f11b230845b938049fc6
Dirk Lemstra <dirk@git.imagemagick.org>
2017-04-06 09:01:32 +0200

    Define WINVER to get rid of warning.

--- configure/stdafx.h
+++ configure/stdafx.h
@@ -10,6 +10,8 @@
 #pragma once
 #endif // _MSC_VER > 1000
 
+#define WINVER 0x0501
+
 #define VC_EXTRALEAN // Exclude rarely-used stuff from Windows headers
 
 #include <afxwin.h>   // MFC core and standard components
