e156aa4c13b6a60dcf1e09d79ffdf1e35f127452
Dirk Lemstra <dirk@lemstra.org>
2021-09-04 11:01:50 +0200

    Added extra null check.

--- configure/Pages/TargetPage.cpp
+++ configure/Pages/TargetPage.cpp
@@ -206,7 +206,7 @@ wstring TargetPage::getEnvironmentVariable(const wchar_t *name)
 
   if (_wdupenv_s(&buffer,&length,name) == 0)
   {
-    if (length > 0)
+    if ((buffer != (wchar_t *) NULL) && (length > 0))
     {
       value=wstring(buffer);
       free(buffer);
